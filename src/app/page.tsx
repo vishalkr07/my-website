f35b3fb81cd14614a9551a5e621179f18a6c00cb
root <root@beast.localdomain>
2023-07-23 14:25:42 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles/Home.module.css";
+import Switch from "@mui/material/Switch";
+
+const label = { inputProps: { "aria-label": "Switch demo" } };
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  );
+}
