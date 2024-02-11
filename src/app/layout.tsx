724e169e241a883f2b08d61fc39e3fcab7fe45b1
Vishal Kumar <vishalkr.k07@gmail.com>
2024-02-12 01:02:45 +0530

    Configure vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import './styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from './Components/NavBar'
+import "./styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import NavBar from "./Components/NavBar";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Vishal Kumar',
-  description: 'Vishal Kumar',
-}
+  title: "Vishal Kumar",
+  description: "Vishal Kumar",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        {children}</body>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
